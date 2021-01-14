28cd69e44fe20d90218ea3a88fe70a4914a8fc9d
arielmirra <amirra@mulesoft.com>
2021-01-14 12:06:25 -0300

    APIMF-2770: improve sidebar structure

---
+++ website/sidebars.js
@@ -0,0 +1,27 @@
+module.exports = {
+    docs: {
+        AMF: [
+            'AMF/amf_intro',
+            {
+                'Using AMF': ['AMF/using-amf/amf_setup']
+            }
+        ],
+        Tutorials: [
+            'modularization',
+            'custom_validations',
+            'queries'
+        ],
+        'Code Samples': [
+            'code/parsing_code',
+            'code/generation_code',
+            'code/validation_code',
+            'code/resolution_code',
+            'code/custom_validation_code'
+        ],
+        AML: [
+            'aml',
+            'aml_spec',
+            'aml_validation_model'
+        ]
+    }
+};
